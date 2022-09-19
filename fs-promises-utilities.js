ad5ee8c99f391364e56e44f92211c56165f4fd74
Michael Wood <michaelwood33311@icloud.com>
2022-09-19 08:55:02 -0400

    added reading of text file using fs-promises

---
+++ fs-promises-utilities.js
@@ -0,0 +1,14 @@
+const fs = require('fs/promises');
+
+
+// reading a text file using fs-promises
+
+async function example() {
+  try {
+    const data = await fs.readFile('/Users/joe/test.txt', { encoding: 'utf8' });
+    console.log(data);
+  } catch (err) {
+    console.log(err);
+  }
+}
+example();

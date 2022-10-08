593506602cb5aa578099cfa73ddf2b96755ce70e
Michael Wood <michaelwood33311@icloud.com>
2022-10-08 12:22:15 -0400

    added node TCP server libary

--- axios-utilities.js
+++ axios-utilities.js
@@ -2,20 +2,24 @@ const fs = require('fs')
 const path = require('path')  
 const axios = require('axios')
 
-const downloadAxiosFile = async (url, filePath) => {  
-  
-  const writer = fs.createWriteStream(filePath)
-
-  const response = await axios({
-    url,
-    method: 'GET',
-    responseType: 'stream'
-  })
-
-  response.data.pipe(writer)
-
-  return new Promise((resolve, reject) => {
+const axiosDownloadFile = async (url, filePath) => {  
+  return new Promise(async (resolve, reject) => {
+    const writer = fs.createWriteStream(filePath)
     writer.on('finish', resolve)
     writer.on('error', reject)
+
+    let response;
+    try {
+       response = await axios({
+        url,
+        method: 'GET',
+        responseType: 'stream'
+      })
+    } catch (e) {
+      reject(e);
+      return;
+    }
+  
+    response.data.pipe(writer)
   })
 }

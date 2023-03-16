697832ebc3e8f41a1f68a6e394e549a2a117ecba
Michael Wood <michaelwood33311@icloud.com>
2023-03-16 08:42:18 -0400

    added utility to parse stringified objects that have functions

--- JSON Utilities/stringify-objects-with-functions.js
+++ JSON Utilities/stringify-objects-with-functions.js
@@ -12,3 +12,12 @@ const test = {
 
 const str = JSON.stringify(test, (key, value) => typeof value === 'function' ? `funcxyx_${value.toString()}` : value, 4);
 
+const par = JSON.parse(str, (key, value) => {
+    if (typeof value === 'string' && value.startsWith('funcxyx_')) {
+        const func = value.substring(8);
+        return eval(func);
+    }
+    return value;
+})
+
+console.log(par.e.h('Michael'))
